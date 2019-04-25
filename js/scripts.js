b975f11d7ef8d169e5f193722df30f1f03d46e8e
allthesignals <wilburnforce@gmail.com>
2019-04-25 15:32:24 -0400

    Add functionality sort by project attrs dynamically

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,47 @@
 // load projects data
 $.getJSON('./data/projects.json', function(projects) {
-  // iterate over array of projects objects
-  projects.forEach(function(project) {
-    // build out a list item with project info
-    const listItem = `
-      <div class="list-item">
-        <h3>${project.id}</h3> <br/>
-        <p>${project.description}</p>
-      </div>
-    `;
+  // initially iterate over array of projects objects
+  render(projects);
 
-    // append the list item to the projects list div
-    $('#projects-list').append(listItem);
+  const [someProject] = projects;
+
+  // set up the sort button UI
+  Object.keys(someProject).forEach(function(key, index) {
+    // append each sort button
+    $('#project-sort-buttons').append(`
+      <button id="project-${key}">${key}</button>
+    `);
+
+    // bind a click event to each sort button
+    $(`#project-${key}`).on('click', sortBy.bind(null, key, projects));
   });
 });
+
+// main renderer function - takes projects and selector
+function render(projects, projectListSelector='#projects-list') {
+  const listItems = projects.map(function(project, index) {
+    // call the list item template function, pass in a project
+    // this function is provided globally in the index.html
+    return renderListItemTemplate(project);
+  });
+
+  // replace the project list
+  $(projectListSelector).html(listItems);
+}
+
+// ability to sort projects by key
+function sortBy(key, projects) {
+  // instance variable to contain state of sorting directionn
+  this.isAscending = !this.isAscending;
+
+  // sort projects by value of given key
+  const sortedProjects = projects.sort((a,b) => {
+    if (this.isAscending) {
+      return a[key] > b[key];
+    } else {
+      return a[key] < b[key];
+    }
+  });
+
+  render(sortedProjects);
+}
