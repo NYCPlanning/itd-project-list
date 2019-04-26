1b7a27f1bbea10671c3de23c8a9c6917b6192487
aferrar <aferrar90@gmail.com>
2019-04-26 14:56:27 -0400

    added tooltips, formated meta labels

--- js/template.js
+++ js/template.js
@@ -1,24 +1,32 @@
 function renderListItemTemplate(project) {
   const requiresCityNet = project.cityNet ? 'red' : 'silver';
-  const cityNetToolTip = project.cityNet ? 'Requires CityNet' : 'Does Not Require CityNet';
+  const cityNetToolTip = project.cityNet ? 'Requires CityNet' : 'CityNet Not Required';
+  const publicProduct = project.publicProduct ? 'green-dark' : 'silver';
+  const issueToolTip = project.issueTracker ? 'Issue Tracker Link' : 'No Issue Tracker'
+  const publicProductToolTip = project.publicProduct ? 'Has Public Product' : 'No Public Product';
   const repoDocsIcon = project.repoDocs ? 'check' : 'times';
   const repoDocsColor = project.repoDocs ? 'green-dark' : 'red-dark';
   const docsToolTip = project.repoDocs ? 'Has Docs' : 'No Docs';
-  const repoLink = project.repoLink ? `<a href="${project.publicRepo}"><i class="fab fa-git-square"></i></a>` : '<i class="fab fa-git-square silver"></i>';
+  const repoLink = project.url ? `<a href="${project.url}"><i class="fab fa-git-square"></i></a>` : '<i class="fab fa-git-square silver"></i>';
+  const repoToolTip = project.url ? 'Repository Link' : 'No Repository';
   const issueLink = project.issueTracker ? `<a href="${project.issueTracker}"><i class="fa fa-exclamation-circle"></i></a>` : '<i class="fa fa-exclamation-circle silver"></i>';
   return `
     <div class="list-item">
       <h3 class="project-title">${project.name}</h3>
       <p class="project-description">${project.description}</p>
       <ul class="list-item-meta">
-        <li>${project.category}</li>
-        <li>${project.techOwner}</li>
-        <li>${project.busOwner}</li>
-        <li>${project.language}</li>
-        <li>Website: ${project.url}</li>
-        <li>${repoLink}</li>
-        <li>${issueLink}</li>
-        <li>${project.publicProduct}</li>
+        <li><strong>Category: </strong>${project.category}</li>
+        <li><strong>Tech Owner: </strong> ${project.techOwner}</li>
+        <li><strong>Business Owner: </strong> ${project.busOwner}</li>
+        <li><strong>Language: </strong>${project.language}</li>
+        <li>${repoLink}<span class="tooltip">${repoToolTip}</span></li>
+        <li>${issueLink}<span class="tooltip">${issueToolTip}</span></li>
+        <li>
+          <span class="fa-stack">
+            <i class="fa fa-users ${publicProduct} fa-stack-1x"></i>
+          </span>
+          <span class="tooltip">${publicProductToolTip}</span>
+        </li>
 
         <li>
           <span class="fa-stack">
