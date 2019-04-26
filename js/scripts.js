742f876aa6a85ad7ddf4340bd854260615cfda59
Frank Chin <fchin@planning.nyc.gov>
2019-04-26 12:30:41 -0400

    25 Now sort on chrome

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,7 @@ function render(projects, projectListSelector='#projects-list') {
   const listItems = projects.map(function(project, index) {
     // call the list item template function, pass in a project
     // this function is provided globally in the index.html
+
     return renderListItemTemplate(project);
   });
 
@@ -37,11 +38,19 @@ function sortBy(key, projects) {
   // sort projects by value of given key
   const sortedProjects = projects.sort((a,b) => {
     if (this.isAscending) {
-      return a[key] > b[key];
-    } else {
-      return a[key] < b[key];
+      if (a[key] > b[key]){
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+      else {
+      if (a[key] < b[key]){
+        return 1;
+      } else {
+        return -1;
+      }
     }
   });
-
   render(sortedProjects);
 }
