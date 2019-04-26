a595f3e675cbd004554789f2ef10e0a6ddb3ad4c
aferrar <aferrar90@gmail.com>
2019-04-26 14:18:35 -0400

    Added tooltips with styling. Added conditional icons and links.

--- js/template.js
+++ js/template.js
@@ -1,28 +1,37 @@
 function renderListItemTemplate(project) {
-  const requiresCityNet = project.requiresCityNet ? 'red' : 'silver';
-
+  const requiresCityNet = project.cityNet ? 'red' : 'silver';
+  const cityNetToolTip = project.cityNet ? 'Requires CityNet' : 'Does Not Require CityNet';
+  const repoDocsIcon = project.repoDocs ? 'check' : 'times';
+  const repoDocsColor = project.repoDocs ? 'green-dark' : 'red-dark';
+  const docsToolTip = project.repoDocs ? 'Has Docs' : 'No Docs';
+  const repoLink = project.repoLink ? `<a href="${project.publicRepo}"><i class="fab fa-git-square"></i></a>` : '<i class="fab fa-git-square silver"></i>';
+  const issueLink = project.issueTracker ? `<a href="${project.issueTracker}"><i class="fa fa-exclamation-circle"></i></a>` : '<i class="fa fa-exclamation-circle silver"></i>';
   return `
     <div class="list-item">
       <h3 class="project-title">${project.name}</h3>
       <p class="project-description">${project.description}</p>
       <ul class="list-item-meta">
-        <li>web app</li>
-        <li>edm</li>
-        <li>capital planning</li>
-        <li>Javascript</li>
-        <li>Website: http://</li>
-        <li>Repo: http://</li>
-        <li>Issues: http://</li>
-        <li>CityNet
+        <li>${project.category}</li>
+        <li>${project.techOwner}</li>
+        <li>${project.busOwner}</li>
+        <li>${project.language}</li>
+        <li>Website: ${project.url}</li>
+        <li>${repoLink}</li>
+        <li>${issueLink}</li>
+        <li>${project.publicProduct}</li>
+
+        <li>
           <span class="fa-stack">
             <i class="fa fa-network-wired ${requiresCityNet} fa-stack-1x"></i>
           </span>
+          <span class="tooltip">${cityNetToolTip}</span>
         </li>
-        <li>Documented
+        <li>
           <span class="fa-stack">
             <i class="fa fa-file fa-lg silver fa-stack-1x"></i>
-            <i class="fa fa-check fa-xs green-dark fa-stack-1x"></i>
+            <i class="fa fa-${repoDocsIcon} fa-xs ${repoDocsColor} fa-stack-1x"></i>
           </span>
+          <span class="tooltip">${docsToolTip}</span>
         </li>
       </ul>
 
